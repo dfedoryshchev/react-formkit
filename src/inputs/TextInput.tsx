9a3b7c1800ec2339476b69d6399c1694b0b2b67e
Dmitriy Fedoryshchev <dmitriy.fedoryshchev@gmail.com>
2024-09-14 11:48:00 +0300

    refactor: TextInput as typed component

--- src/inputs/TextInput.tsx
+++ src/inputs/TextInput.tsx
@@ -1,14 +1,22 @@
-import React from 'react'
+import React, { forwardRef } from 'react'
+import { BaseInputProps } from '../types'
 
-const TextInput = ({ value, onChange, placeholder }) => {
-    return (
-        <input
-            type="text"
-            value={value || ''}
-            onChange={e => onChange(e.target.value)}
-            placeholder={placeholder}
-        />
-    )
-}
+const TextInput = forwardRef<HTMLInputElement, BaseInputProps>(
+    ({ value, onChange, placeholder, disabled, className }, ref) => {
+        return (
+            <input
+                ref={ref}
+                type="text"
+                value={value || ''}
+                onChange={e => onChange(e.target.value)}
+                placeholder={placeholder}
+                disabled={disabled}
+                className={className}
+            />
+        )
+    }
+)
+
+TextInput.displayName = 'TextInput'
 
 export default TextInput
