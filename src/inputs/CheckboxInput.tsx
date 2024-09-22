8234155dd8b9ee8fd5f010782f041d5d8eb80a90
Dmitriy Fedoryshchev <dmitriy.fedoryshchev@gmail.com>
2024-09-22 15:14:00 +0300

    add checkbox input

---
+++ src/inputs/CheckboxInput.tsx
@@ -0,0 +1,30 @@
+import React, { forwardRef } from 'react'
+
+interface CheckboxInputProps {
+    checked: boolean
+    onChange: (checked: boolean) => void
+    label?: string
+    disabled?: boolean
+    className?: string
+}
+
+const CheckboxInput = forwardRef<HTMLInputElement, CheckboxInputProps>(
+    ({ checked, onChange, label, disabled, className }, ref) => {
+        return (
+            <label className={className}>
+                <input
+                    ref={ref}
+                    type="checkbox"
+                    checked={!!checked}
+                    onChange={e => onChange(e.target.checked)}
+                    disabled={disabled}
+                />
+                {label && <span>{label}</span>}
+            </label>
+        )
+    }
+)
+
+CheckboxInput.displayName = 'CheckboxInput'
+
+export default CheckboxInput
