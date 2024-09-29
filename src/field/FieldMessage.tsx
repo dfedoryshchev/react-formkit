3288b7ad7823aa340f0bea47ed810a68716f7318
Dmitriy Fedoryshchev <dmitriy.fedoryshchev@gmail.com>
2024-09-29 14:22:00 +0300

    add FieldMessage component

---
+++ src/field/FieldMessage.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './FieldMessage.scss'
+
+type MessageType = 'error' | 'warning' | 'info'
+
+interface FieldMessageProps {
+    message?: string
+    type?: MessageType
+    visible?: boolean
+}
+
+const FieldMessage: React.FC<FieldMessageProps> = ({
+    message,
+    type = 'error',
+    visible = true,
+}) => {
+    if (!message || !visible) return null
+
+    return (
+        <div className={`custom-field-message custom-field-message--${type}`}>
+            <span>{message}</span>
+        </div>
+    )
+}
+
+export default FieldMessage
