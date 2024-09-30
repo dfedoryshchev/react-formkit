d86edede56eb741ffcc715df73e00eeb30b20272
Dmitriy Fedoryshchev <dmitriy.fedoryshchev@gmail.com>
2024-09-30 23:18:00 +0300

    wip: withControlProps hoc

---
+++ src/field/hocs/withControlProps.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+// strips field-level props so only control-relevant props reach the input
+const FIELD_PROPS = [
+    'label',
+    'required',
+    'errorMessage',
+    'warningMessage',
+    'name',
+    'rules',
+] as const
+
+type FieldPropKey = (typeof FIELD_PROPS)[number]
+
+export const withControlProps = <P extends object>(
+    WrappedComponent: React.ComponentType<P>,
+) => {
+    const WithControlProps = React.forwardRef<unknown, P & Record<FieldPropKey, any>>(
+        (props, ref) => {
+            const controlProps = { ...props } as any
+            for (const key of FIELD_PROPS) {
+                delete controlProps[key]
+            }
+            return <WrappedComponent {...controlProps} ref={ref} />
+        },
+    )
+
+    WithControlProps.displayName = `withControlProps(${WrappedComponent.displayName || WrappedComponent.name || 'Component'})`
+    return WithControlProps
+}
