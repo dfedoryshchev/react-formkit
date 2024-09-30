27b71520ba4aa254a22a710740cc563843260e29
Dmitriy Fedoryshchev <dmitriy.fedoryshchev@gmail.com>
2024-09-30 20:37:00 +0300

    wip: hoc pattern for field composition

---
+++ src/field/hocs/withFieldMessage.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import FieldMessage from '../FieldMessage'
+
+interface WithFieldMessageProps {
+    errorMessage?: string
+    warningMessage?: string
+}
+
+export const withFieldMessage = <P extends object>(
+    WrappedComponent: React.ComponentType<P>,
+) => {
+    const WithFieldMessage = React.forwardRef<unknown, P & WithFieldMessageProps>(
+        (props, ref) => {
+            const { errorMessage, warningMessage, ...rest } = props as any
+
+            return (
+                <>
+                    <WrappedComponent {...(rest as P)} ref={ref} />
+                    <FieldMessage
+                        message={errorMessage || warningMessage}
+                        type={errorMessage ? 'error' : 'warning'}
+                        visible={!!(errorMessage || warningMessage)}
+                    />
+                </>
+            )
+        },
+    )
+
+    WithFieldMessage.displayName = `withFieldMessage(${WrappedComponent.displayName || WrappedComponent.name || 'Component'})`
+    return WithFieldMessage
+}
---
+++ src/field/hocs/withLabel.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Label from '../../components/Label'
+
+interface WithLabelProps {
+    label?: string
+    required?: boolean
+}
+
+export const withLabel = <P extends WithLabelProps>(
+    WrappedComponent: React.ComponentType<P>,
+) => {
+    const WithLabel = React.forwardRef<unknown, P & WithLabelProps>((props, ref) => {
+        const { label, required, ...rest } = props as any
+
+        if (!label) {
+            return <WrappedComponent {...(rest as P)} ref={ref} />
+        }
+
+        return (
+            <Label text={label} required={required}>
+                <WrappedComponent {...(rest as P)} ref={ref} />
+            </Label>
+        )
+    })
+
+    WithLabel.displayName = `withLabel(${WrappedComponent.displayName || WrappedComponent.name || 'Component'})`
+    return WithLabel
+}
