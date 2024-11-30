c45973f10c6f905f03368bcb77d93ce51fa28fe6
Dmitriy Fedoryshchev <dmitriy.fedoryshchev@gmail.com>
2024-11-30 11:37:00 +0200

    add field type definitions

---
+++ src/field/field.types.ts
@@ -0,0 +1,25 @@
+import { ControlProps } from '../controls/control.types'
+
+export interface FieldProps extends Omit<ControlProps, 'value' | 'onChange'> {
+    name: string
+    label?: string
+    required?: boolean
+    defaultValue?: unknown
+}
+
+export interface InnerFieldProps {
+    value: unknown
+    onChange: (value: unknown) => void
+    onBlur: () => void
+    errorMessage?: string
+    name: string
+    ref?: React.Ref<unknown>
+}
+
+export interface MetadataProps {
+    label?: string
+    required?: boolean
+    type: string
+}
+
+export type WrappedControlProps = InnerFieldProps & MetadataProps & Record<string, unknown>
