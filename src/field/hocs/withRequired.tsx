49a1a962c12e0870e0af0296540f4bee362190d4
Dmitriy Fedoryshchev <dmitriy.fedoryshchev@gmail.com>
2026-03-10 20:14:37 +0000

    refactor(field): drop eslint-disable + as any in withRequired

--- src/field/hocs/withRequired.tsx
+++ src/field/hocs/withRequired.tsx
@@ -10,12 +10,15 @@ export const withRequired = <P extends WithRequiredProps>(
     WrappedComponent: React.ComponentType<P>,
 ) => {
     const WithRequired = React.forwardRef<unknown, P>((props, ref) => {
-        const { name, required: explicitRequired } = props as any
+        const { name, required: explicitRequired } = props
         const schemaRequired = useIsFieldRequired(name || '')
         const isRequired = explicitRequired ?? schemaRequired
 
-        // eslint-disable-next-line
-        return <WrappedComponent {...(props as any)} required={isRequired} ref={ref} />
+        // Merging `required` into generic props P cannot be expressed without a
+        // cast (P could be a narrower subtype); confined to this one boundary.
+        const mergedProps = { ...props, required: isRequired } as P
+        const Wrapped = WrappedComponent as React.ComponentType<P & React.RefAttributes<unknown>>
+        return <Wrapped {...mergedProps} ref={ref} />
     })
 
     WithRequired.displayName = `withRequired(${WrappedComponent.displayName || WrappedComponent.name || 'Component'})`
